d7d19e3e1009a99e5495f09f3321000877340a11
Mateusz Olczyk <matik60@gmail.com>
2021-02-12 18:04:19 +0100

    Fixed tests for msbuild

--- test/avl_tree.c
+++ test/avl_tree.c
@@ -5,7 +5,7 @@
 #include "unity.h"
 
 void setUp(void) {
-    static uint8_t memory_pool[2000];
+    static uint8_t memory_pool[3000];
     cmagic_memory_init(memory_pool, sizeof(memory_pool));
 }
 
